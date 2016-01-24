6f59076e69bec39bc3a0666d97d8ee923c2341ef
SashaFlaska <alexander.flaska@ucdenver.edu>
2016-01-24 11:43:38 -0700

    Sasha Flaska
    PA0

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@
  */
 #include <iostream>
 
-int main() {
-    // TODO Substitute 11 with your UCD ID number, modulo 13 (myId % 13)
-    std::cout << "Hello, world! My number is " << 11 << '!' << std::endl;
+int main()
+{
+    int myNumber=105757935%13;
+    std::cout << "Hello, world! My number is " << myNumber << '!' << std::endl;
 
     return 0;
 }
